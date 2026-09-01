5d56ed9746426b0c9e3e8f99df874d571c71cbe7
Kamaal Farah <kamaal.f1@gmail.com>
2026-09-01 21:35:59 +0200

    Export typed route response helper
    **Summary**
    Consumers can import the route response type from the package API.
    
    **Changes**
    Re-export RouteConfigToTypedResponse so consumers can derive types from
    documented route response configurations.
    
    Signed-off-by: Kamaal Farah <kamaal.f1@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { $, StandardOpenAPIHono } from './app.ts';
 export { allOf, objectSchema } from './compose.ts';
 export { createRoute } from './route.ts';
+export type { RouteConfigToTypedResponse } from './type-inference.ts';
 export type { SchemaOrReference } from './types.ts';
